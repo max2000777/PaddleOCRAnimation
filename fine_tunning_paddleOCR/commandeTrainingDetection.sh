2ada0fbb86d38f977f2c2f8421d9f81d19eaeabf
max2000777 <maxime.brennan@outlook.com>
2025-11-08 09:37:10 +0100

    better configs

--- fine_tunning_paddleOCR/commandeTrainingDetection.sh
+++ fine_tunning_paddleOCR/commandeTrainingDetection.sh
@@ -1,19 +1,22 @@
 PYTHONPATH="$PWD"  \
-    python ./tools/train.py -c ../OCRSub/OCRSub/B2_Segmentation/PP-OCRv5_server_det_anime.yml \
+    python ./tools/train.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/PP-OCRv5_server_det_anime.yml \
     -o Global.pretrained_model=../Data/Modèles/PP-OCRv5_server_det_pretrained.pdparams \
-    Global.epoch_num=5
+    Global.epoch_num=7
 
 PYTHONPATH="$PWD" \
-    python ./tools/train.py -c "../OCRSub/OCRSub/B2_Segmentation/PP-OCRv5_server_det_anime.yml" \
+    python ./tools/train.py -c "../PaddleOCRAnime/fine_tunning_paddleOCR/PP-OCRv5_server_det_anime.yml" \
     -o Global.pretrained_model="../Data/Modeles/PP-OCRv5_server_det_pretrained.pdparams" \
-    Global.epoch_num=50  \
-    Global.checkpoints="/home/maxim/code/SubProject/PaddleOCR/output/PP-OCRv5_server_det_anime/latest/latest" \
-    wandb.allow_val_change=true \
-    wandb.id=k4oy2zem565 \
-    wandb.resume=must
+    Global.epoch_num=5  \
+    Global.checkpoints="/home/maxim/code/SubProject/PaddleOCR_output/PP-OCRv5_server_det_anime/latest/latest" \
 
 # rec
 PYTHONPATH="$PWD"  \
-    python ./tools/train.py -c ../OCRSub/OCRSub/B2_Segmentation/french_PP-OCRv5_server_rec.yml \
+    python ./tools/train.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/french_PP-OCRv5_server_rec.yml \
     -o Global.pretrained_model=../Data/Modèles/PP-OCRv5_server_rec_pretrained.pdparams \
     Global.epoch_num=30
+
+
+PYTHONPATH="$PWD"  \
+    python ./tools/train.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/french_PP-OCRv5_mobile_rec.yml \
+    -o Global.pretrained_model=../Data/Modèles/latin_PP-OCRv5_mobile_rec_pretrained.pdparams \
+    Global.epoch_num=50
