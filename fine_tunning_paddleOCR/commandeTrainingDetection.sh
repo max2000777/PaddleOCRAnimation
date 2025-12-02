093569cc4e7e288657766f3ab24849ee7ed03346
max2000777 <maxime.brennan@outlook.com>
2025-12-02 20:29:03 +0100

    verry good params

--- fine_tunning_paddleOCR/commandeTrainingDetection.sh
+++ fine_tunning_paddleOCR/commandeTrainingDetection.sh
@@ -3,7 +3,7 @@ PYTHONPATH="$PWD"  \
     -o Global.pretrained_model="../Data/Modèles/PP-OCRv5_server_det_pretrained.pdparams" \
     Global.epoch_num=7
 
-PYTHONPATH="$PWD" \
+PYTHONPATH="$PWD"  \
     python ./tools/train.py -c "../PaddleOCRAnime/fine_tunning_paddleOCR/PP-OCRv5_server_det_anime.yml" \
     -o Global.pretrained_model="../Data/Modèles/PP-OCRv5_server_det_pretrained.pdparams" \
     Global.epoch_num=3  \
@@ -19,5 +19,8 @@ PYTHONPATH="$PWD"  \
 PYTHONPATH="$PWD"  \
     python ./tools/train.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/french_PP-OCRv5_mobile_rec.yml \
     -o Global.pretrained_model=../Data/Modèles/latin_PP-OCRv5_mobile_rec_pretrained.pdparams \
-    Global.epoch_num=15 \
+    Global.epoch_num=20 \
     Global.checkpoints="/home/maxim/code/SubProject/PaddleOCR_output/french_PP-OCRv5_mobile_rec/latest/latest"
+
+
+
