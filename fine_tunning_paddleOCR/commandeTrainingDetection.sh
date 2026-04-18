eea85d9dbd7da23a0f2ef01cbcc42e5e5c6af0c8
max2000777 <maxime.brennan@outlook.com>
2026-04-18 14:12:14 +0200

    eval

--- fine_tunning_paddleOCR/commandeTrainingDetection.sh
+++ fine_tunning_paddleOCR/commandeTrainingDetection.sh
@@ -13,6 +13,7 @@ PYTHONPATH="$PWD"  \
 PYTHONPATH="$PWD"  \
     python ./tools/train.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/PP-OCRv5_mobile_det.yml \
     -o Global.pretrained_model=../Data/Modèles/PP-OCRv5_mobile_det_pretrained.pdparams \
+    Global.checkpoints="/home/maxim/code/SubProject/PaddleOCR_output/PP-OCRv5_mobile_det_anime/latest/latest" \
     Global.epoch_num=4
 
 
@@ -26,9 +27,18 @@ PYTHONPATH="$PWD"  \
 PYTHONPATH="$PWD"  \
     python ./tools/train.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/french_PP-OCRv5_mobile_rec.yml \
     -o Global.pretrained_model=../Data/Modèles/latin_PP-OCRv5_mobile_rec_pretrained.pdparams \
-    Global.epoch_num=20 \
-    Global.checkpoints="/home/maxim/code/SubProject/PaddleOCR_output/french_PP-OCRv5_mobile_rec/latest/latest"
+    Global.checkpoints="/home/maxim/code/SubProject/PaddleOCR_output/french_PP-OCRv5_mobile_rec/best_accuracy/best_accuracy"
 
+
+# EVAL
+#Det
+python3 tools/eval.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/PP-OCRv5_server_det_anime.yml  \
+    -o Global.pretrained_model=/home/maxim/code/SubProject/PaddleOCR_output/PP-OCRv5_server_det_anime/latest/latest   \
+    Eval.dataset.data_dir=../PaddleOCRAnime/dataset     Eval.dataset.label_file_list='[../PaddleOCRAnime/dataset/detTest.txt]'
+
+# Rec
+python3 tools/eval.py -c ../PaddleOCRAnime/fine_tunning_paddleOCR/french_PP-OCRv5_mobile_rec.yml -o \
+    Global.pretrained_model=/home/maxim/code/SubProject/PaddleOCR_output/french_PP-OCRv5_mobile_rec/latest/latest
 
 
 python "C:\code\VideOCR\CLI\videocr_cli.py" --video_path "D:\Téléchargement\Banner of the stars\Banner Of The Stars II\01.avi" --use_server_model true --lang fr --output "C:\code\VideOCR\CLI\01.srt" --use_dual_zone true --use_gpu true --text_recognition_batch_size 200 --crop_x 6 --crop_y 275 --crop_width 705 --crop_height 117 --crop_x2 3 --crop_y2 0 --crop_width2 705 --crop_height2 85
